8324509c2fb54cffde71b59a7d982c3da59d3ff0
nihadjafarov01 <mi72kv3d0@code.edu.az>
2023-10-31 23:09:43 +0400

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ConsoleApp1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Student student = new Student();
+        //   Console.WriteLine(student); ("Nihad  Jafarov", "P101", 20)
+        char option = '0';
+        Console.WriteLine("1. Create student \nq. quit");
+        do
+        {
+            Console.WriteLine("1, 2, 3 or q");
+            option = Convert.ToChar(Console.ReadLine());
+            switch (option)
+            {
+                case '1':
+                    Student student = new Student();
+                    do
+                    {
+                        Console.Write("Fullname: ");
+                        student.FullName = Console.ReadLine();
+                    }
+                    while (Student.CheckFullname(student.FullName) == false);
+                    do
+                    {
+                        Console.Write("Group no: ");
+                        student.GroupNo = Console.ReadLine();
+                    }
+                    while (Student.CheckGroupNo(student.GroupNo) == false);
+                    do
+                    {
+                        Console.Write("Age: ");
+                        student.Age = Convert.ToInt32(Console.ReadLine());
+                    }
+                    while (student.Age == 0);
+                    Console.WriteLine(student);
+                    break;
+            }
+        }
+        while (option != 'q');
+        //Console.WriteLine(Student.CheckGroupNo(student.GroupNo));
+        //Console.WriteLine(Student.CheckFullname(student.FullName));
+    }
+}
---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,107 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ConsoleApp1;
+
+internal class Student
+{
+    public static int id;
+    public int Id { get; }
+    public  string fullName;
+    public  string FullName 
+    {
+        get => fullName;
+        set
+        {
+            value = value.Trim();
+            int index = 0;
+            int counter = 0;
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (value[i] == ' ')
+                {
+                    index = i;
+                    counter++;
+                }
+            }
+            if (counter == 1) 
+            {
+                if (value[0] == Char.ToUpper(value[0]) && value[index + 1] == Char.ToUpper(value[index + 1]))
+                {
+                    fullName = value;
+                }
+            }
+            else
+            {
+                fullName = null;
+            }
+        }
+    }
+    public string groupNo;
+    public string GroupNo 
+    {
+        get => groupNo;
+        set
+        {
+            if (value[0] == Char.ToUpper(value[0]))
+            {
+                if (Convert.ToInt32(value.Substring(1)) > 99 && Convert.ToInt32(value.Substring(1)) < 1000)
+                {
+                    groupNo = value;
+                    return;
+                }
+            }
+            groupNo = null;
+        }
+    }
+    public int age;
+    public int Age 
+    { 
+        get => age; 
+        set
+        {
+            if (value > 0)
+            {
+                age = value;
+            }
+            else
+            {
+                age = 0;
+            }
+        }
+    }
+
+    public static bool CheckGroupNo(string groupNo)
+    {
+        if (String.IsNullOrEmpty(groupNo))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    public static bool CheckFullname(string fullName)
+    {
+        if (String.IsNullOrEmpty(fullName))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    public Student(/*string fullName, string groupNo, int age*/) 
+    {
+        //FullName = fullName;
+        //GroupNo = groupNo;
+        //Age = age;
+        id++;
+        Id = id;
+    }
+    public override string ToString()
+    {
+        return $"{Id} --> {FullName}, {GroupNo}, {Age}";
+    }
+}
